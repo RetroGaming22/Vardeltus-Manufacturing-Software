153d7c335992b48a1907f372cc8d4fcf6020f78c
RetroGaming22 <126254358+RetroGaming22@users.noreply.github.com>
2024-08-22 11:47:59 -0400

    FEAT - Added preview object
    - Added preview object to show what the user is about to input
    - Added GNU GPL disclaimer
    - Clear Color has been reverted to black as there is no need for an invisible line
    - Combined all drawObjects functions (ex. drawLines) into a single drawObjects function (had to add more code to the draw rectangles segments as they had the same relative point issue as discussed in the selObjects function before the rectangle points declaration where assumptions of their relative position lead to issues if the assumptions are incorrect.)

--- src/main.zig
+++ src/main.zig
@@ -1,5 +1,6 @@
 // VMS is built using raylib-zig (c) Nikolas Wipper 2023
 // As well as using the zig language by Andrew Kelly
+// Vardeltus Manufacturing Softare - Copyright (C) 2024 RetroGaming22\nThis program comes with ABSOLUTELY NO WARRANTY and this is free software; you are welcome to redistribute it under certain conditions. Look in the LICENSE file or visit https://www.gnu.org/licenses/#GPL for more details.
 
 // Zig definitions
 const std = @import("std");
@@ -10,19 +11,24 @@ const stdin = std.io.getStdIn();
 // Program variables
 /// An object with two points
 const Object2 = struct {
+    /// The type of object (Rectangle, Line, Circle, etc)
     type: u8,
+    /// The first point of the object
     p1: rl.Vector2,
+    /// The second point of the object
     p2: rl.Vector2,
+    /// The color of the object
     color: rl.Color,
-    // This exists so that I am able to determine if it has a color and then giving it one if it doesn't in the colorInterpolater
+    /// This exists so that I am able to determine if it has a color and then giving it one if it doesn't in the colorInterpolater
     colored: bool,
+    /// Whether or not the object is selected
     selected: bool,
-    // A value used to mark p1 to be overwritten
+    /// A value used to mark p1 (and thus the object as a whole) to be overwritten
     overwrite: bool,
 };
 
-// This (objects being stored in an array) seems really bad, but I am not sure by how much or how to make it better at the moment.
-/// Stores the points for lines
+// This (objects being stored in an array) seems really bad, so I might change it to be done with more dynmaic memory allocation later.
+/// Stores the points for objects
 var objects: [128]Object2 = undefined;
 /// Stores the number of entries in objects
 var index: u8 = 0;
@@ -41,13 +47,13 @@ const gridLineThickness: f16 = 2;
 const gridSpacing: u16 = 50;
 /// The default color
 const defaultColor: rl.Color = rl.Color.blue;
-/// The color of the selected lines
+/// The color of the selected objects
 const selColor: rl.Color = rl.Color.white;
 /// The color of the grid lines
 const gridColor: rl.Color = rl.Color.gray;
-/// Clears screen.
+/// Clears screen
 const clearKey: rl.KeyboardKey = rl.KeyboardKey.key_delete;
-/// Removes last inputted object.
+/// Removes last inputted object
 const backKey: rl.KeyboardKey = rl.KeyboardKey.key_backspace;
 /// The button used to select objects
 const selButton: rl.MouseButton = rl.MouseButton.mouse_button_left;
@@ -65,27 +71,31 @@ const rectangleModeKey: rl.KeyboardKey = rl.KeyboardKey.key_r;
 const defaultMode: u8 = 'l';
 /// The number of pixels that create an area where an object is considered selected if clicked
 const selectionThreshold: i32 = 6;
-// Draws a grid
+/// Draws a grid if true
 const grid: bool = true;
 /// The size of the text displayed
 const fontSize: i32 = 20;
 /// The color of the text displayed
 const fontColor: rl.Color = rl.Color.white;
-/// If true, when removing last line, instead of carrying the last inputted color, the function will not change the order of the object colors.
+/// If true, when removing last object, instead of carrying the last inputted color, the function will not change the order of the object colors.
 // For example, if you had an orange and green line and was about to create a purple one, but you decided that the green line was not what you wanted. By default, it would make it so that you had the orange line and then the next object you made would be purple. With this setting set to true, the next object would be green, and then the one after that would be purple, which would then carry on until the next color is inputted.
 const retainColors: bool = false;
 /// Will deselect all objects currently selected.
 const clearSelKey: rl.KeyboardKey = rl.KeyboardKey.key_backslash;
+/// The alpha of the object preview color
+const previewColorAlphaPercent: f32 = 60;
+/// The object that you are about to draw
+var previewObject: Object2 = undefined;
 
 // Custom hardcoded values:
-/// A clear "color"
-const clear: rl.Color = rl.Color.init(0, 0, 0, 0);
 // The screen and display values will be figured out later, but now they are hardcoded.
 const screenWidth = 1200;
 const screenHeight = 600;
 
 pub fn main() anyerror!void {
     // Initialization
+
+    // Creates window
     rl.initWindow(screenWidth, screenHeight, "Vardeltus Manufacturing Software");
 
     // Disables esc from closing the program
@@ -94,14 +104,14 @@ pub fn main() anyerror!void {
     // Closes Window when finished
     defer rl.closeWindow();
 
-    // Sets FPS to 60
+    // Sets program to run at 60 FPS
     rl.setTargetFPS(60);
 
     // Main loop
     while (!rl.windowShouldClose()) {
         // Update
 
-        // Gets current pressed key. You can't call the function multiple times per frame so this is handy.
+        // Gets current pressed key. You can't call the function mmiple times per frame so this is handy.
         const currentPressedKey: rl.KeyboardKey = rl.getKeyPressed();
 
         // Will change the mode if the user requests it and also displays the current mode in the bottom left corner
@@ -111,7 +121,7 @@ pub fn main() anyerror!void {
         clearScreen();
         removeLastObject();
 
-        // Checks to see if the user wants to change the line color and then stores it in the color field in the current object
+        // Checks to see if the user wants to change the object color and then stores it in the color field in the current object
         colorEncoder(currentPressedKey);
 
         // Clears the background to be drawn on.
@@ -120,15 +130,14 @@ pub fn main() anyerror!void {
         // Draws a grid in the background if grid = true
         drawGrid();
 
-        // Writes the points and type for objects and then increments the index
+        // Writes the points and type for objects and preview object; then increments the index
         try storeObjectPoints();
 
-        // try for debug messages
-        // Determines if any of the lines have been selected and marks them as so
-        try selObjects();
-
-        // "Fills in" the colors that were not directly written
+        // "Fills in" the colors that were not directly written - NTS: Make sure that this is called after storeObjectPoints because it messes with p1 in storeObjectsPooints.
         colorInterpolater();
+
+        // Determines if any of the objects have been selected and marks them as so
+        selObjects();
 
         // Begins raylib drawing
         rl.beginDrawing();
@@ -136,14 +145,9 @@ pub fn main() anyerror!void {
         defer rl.endDrawing();
 
         // Draws the objects
-        drawLines();
-        drawCircles();
-        drawRectangles();
-
-        // Draws the selected objects as being selected
-        drawSelLines();
-        drawSelCircles();
-        drawSelRectangles();
+        drawObjects();
+        // Draws the preview object
+        try drawObjectPreview();
     }
 }
 
@@ -183,7 +187,7 @@ fn colorEncoder(key: rl.KeyboardKey) void {
             objects[index].colored = true;
         },
         rl.KeyboardKey.key_nine => {
-            objects[index].color = clear;
+            objects[index].color = rl.Color.black;
             objects[index].colored = true;
         },
         // Doesn't do anything if none of the keys are pressed.
@@ -193,14 +197,26 @@ fn colorEncoder(key: rl.KeyboardKey) void {
 
 /// "Fills in" the colors that were not directly written
 fn colorInterpolater() void {
+    // Sets the first color to the default if none is provided by the user
+    if (!objects[0].colored) {
+        objects[0].color = defaultColor;
+        objects[0].colored = true;
+    }
+
     if (index >= 1) {
-        if (!objects[0].colored) objects[0].color = defaultColor;
+        // Goes through the objects and sets the color if it isn't colored already
         for (1..index) |i| {
             if (!objects[i].colored) {
                 objects[i].color = objects[i - 1].color;
                 objects[i].colored = true;
             }
         }
+    }
+    // Sets the preview object color to the last color but with a different alpha value
+    if (objects[index].colored) {
+        previewObject.color = rl.colorAlpha(objects[index].color, (previewColorAlphaPercent / 100));
+    } else {
+        previewObject.color = rl.colorAlpha(objects[index - 1].color, (previewColorAlphaPercent / 100));
     }
 }
 
@@ -224,7 +240,7 @@ fn removeLastObject() void {
         objects[index - 1].selected = false;
 
         if (retainColors) {
-            // Tells the program that the line has been removed
+            // Tells the program that the object has been removed
             index -= 1;
             // Exits the function before the code that makes it so that the color is not retained is not run
             return;
@@ -238,22 +254,36 @@ fn removeLastObject() void {
         for (index + 1..objects.len) |i| {
             objects[i].colored = false;
         }
-        // Tells the program that the line has been removed
+        // Tells the program that the object has been removed
         index -= 1;
     }
 }
 
-/// Writes the points and type for the objects and then increments the index
+/// Writes the points and type for the objects and preview object; then increments the index
 fn storeObjectPoints() !void {
     // Stops program from storing points when user is in selection mode
     if (mode == 's') return;
+
+    // A varible to avoid checking if objects.p1.x doesn't exist twice because zig makes it no longer undefined after it is checked
+    var noP1: bool = undefined;
+    if (objects[index].p1.x == undefined) noP1 = true else noP1 = false;
+
+    // Displays a preview of the shape you are about to draw if the user already gave an input
+    if (!noP1) {
+        // Carries the first point to the next index value in order to avoid the issue of the object corresponding to the index value switching between the preview object and the last inputted object. (Which has issues with color)
+        previewObject.p1 = objects[index].p1;
+        // Sets the second point of the preview object to the current mouse position
+        previewObject.p2 = rl.getMousePosition();
+        // Sets the preview object type to the current mode
+        previewObject.type = mode;
+    }
     if (rl.isMouseButtonPressed(rl.MouseButton.mouse_button_left)) {
         if (index == (objects.len - 1)) {
             // Debug statement telling the user if it is at max capacity
             try stdout.print("Input ignored. Index has reached max capacity ({d}).\n", .{index});
         } else {
             // Writes the first point if it doesn't exist or needs to be overwritten
-            if (objects[index].p1.x == undefined or objects[index].overwrite) {
+            if (noP1 or objects[index].overwrite) {
                 objects[index].p1 = rl.getMousePosition();
                 objects[index].overwrite = false;
             } else {
@@ -283,7 +313,7 @@ fn selObjects() !void {
             objects[i].selected = false;
         }
     }
-    // If the selButton is pressed and if there is a line, then it will check to see if anything was selected
+    // If the selButton is pressed and if there is a object, then it will check to see if anything was selected
     if (rl.isMouseButtonPressed(selButton) and index >= 1) {
         const currentPoint: rl.Vector2 = rl.getMousePosition();
 
@@ -350,85 +380,135 @@ fn selObjects() !void {
     }
 }
 
-/// Draws the line objects
-fn drawLines() void {
+fn drawObjects() void {
     if (index >= 1) {
         for (0..index) |i| {
+            // Draws the line objects
             if (objects[i].type == 'l') {
                 rl.drawLineEx(objects[i].p1, objects[i].p2, lineThickness, objects[i].color);
             }
-        }
-    }
-}
 
-/// Draws the selected line objects as being selected
-fn drawSelLines() void {
-    if (index >= 1) {
-        for (0..index) |i| {
-            if (objects[i].selected and objects[i].type == 'l') {
-                rl.drawLineEx(objects[i].p1, objects[i].p2, selLineThickness, selColor);
-            }
-        }
-    }
-}
-
-/// Draws the circle objects
-fn drawCircles() void {
-    if (index >= 1) {
-        for (0..index) |i| {
+            // Draws the circle objects
             if (objects[i].type == 'c') {
                 const center: rl.Vector2 = objects[i].p1;
                 const radius: f32 = rl.Vector2.distance(objects[i].p1, objects[i].p2);
 
                 rl.drawRing(center, radius - (lineThickness / 2), radius + (lineThickness / 2), 0, 360, 1, objects[i].color);
             }
-        }
-    }
-}
 
-/// Draws the selected circle objects as being selected
-fn drawSelCircles() void {
-    if (index >= 1) {
-        for (0..index) |i| {
+            // Draws the rectangle objects
+            if (objects[i].type == 'r') {
+                var P1X: f32 = undefined;
+                var P1Y: f32 = undefined;
+                var P2X: f32 = undefined;
+                var P2Y: f32 = undefined;
+
+                if (objects[i].p1.x < objects[i].p2.x) {
+                    P1X = objects[i].p1.x;
+                    P2X = objects[i].p2.x;
+                } else {
+                    P1X = objects[i].p2.x;
+                    P2X = objects[i].p1.x;
+                }
+
+                if (objects[i].p1.y < objects[i].p2.y) {
+                    P1Y = objects[i].p1.y;
+                    P2Y = objects[i].p2.y;
+                } else {
+                    P1Y = objects[i].p2.y;
+                    P2Y = objects[i].p1.y;
+                }
+                const width: f32 = P2X - P1X;
+                const height: f32 = P2Y - P1Y;
+
+                rl.drawRectangleLinesEx(rl.Rectangle.init(P1X, P1Y, width, height), lineThickness, objects[i].color);
+            }
+            // Draws the selected line objects as being selected
+            if (objects[i].selected and objects[i].type == 'l') {
+                rl.drawLineEx(objects[i].p1, objects[i].p2, selLineThickness, selColor);
+            }
+            // Draws the selected circle objects as being selected
             if (objects[i].selected and objects[i].type == 'c') {
                 const center: rl.Vector2 = objects[i].p1;
                 const radius: f32 = rl.Vector2.distance(objects[i].p1, objects[i].p2);
                 rl.drawRing(center, radius - (selLineThickness / 2), radius + (selLineThickness / 2), 0, 360, 1, selColor);
             }
-        }
-    }
-}
-
-/// Draws the rectangle objects
-fn drawRectangles() void {
-    if (index >= 1) {
-        for (0..index) |i| {
-            if (objects[i].type == 'r') {
-                const p1X: f32 = objects[i].p1.x;
-                const p1Y: f32 = objects[i].p1.y;
-
-                const width: f32 = objects[i].p2.x - objects[i].p1.x;
-                const height: f32 = objects[i].p2.y - objects[i].p1.y;
-
-                rl.drawRectangleLinesEx(rl.Rectangle.init(p1X, p1Y, width, height), lineThickness, objects[i].color);
-            }
-        }
-    }
-}
-
-/// Draws the selected rectangle objects as being selected
-fn drawSelRectangles() void {
-    if (index >= 1) {
-        for (0..index) |i| {
+            // Draws the selected rectangle objects as being selected
             if (objects[i].selected and objects[i].type == 'r') {
-                const p1X: f32 = objects[i].p1.x;
-                const p1Y: f32 = objects[i].p1.y;
+                var P1X: f32 = undefined;
+                var P1Y: f32 = undefined;
+                var P2X: f32 = undefined;
+                var P2Y: f32 = undefined;
 
-                const width: f32 = objects[i].p2.x - objects[i].p1.x;
-                const height: f32 = objects[i].p2.y - objects[i].p1.y;
+                if (objects[i].p1.x < objects[i].p2.x) {
+                    P1X = objects[i].p1.x;
+                    P2X = objects[i].p2.x;
+                } else {
+                    P1X = objects[i].p2.x;
+                    P2X = objects[i].p1.x;
+                }
 
-                rl.drawRectangleLinesEx(rl.Rectangle.init(p1X, p1Y, width, height), selLineThickness, selColor);
+                if (objects[i].p1.y < objects[i].p2.y) {
+                    P1Y = objects[i].p1.y;
+                    P2Y = objects[i].p2.y;
+                } else {
+                    P1Y = objects[i].p2.y;
+                    P2Y = objects[i].p1.y;
+                }
+                const width: f32 = P2X - P1X;
+                const height: f32 = P2Y - P1Y;
+
+                rl.drawRectangleLinesEx(rl.Rectangle.init(P1X, P1Y, width, height), selLineThickness, selColor);
             }
+        }
+    }
+}
+
+fn drawObjectPreview() !void {
+
+    // Doesn't draw the preview object if user is in selection mode, if the current object was just removed, or if the user hasn't inputted the first point of the object (Otherwise when changing color you would notice that the last object changes color as the preview object is drawn above it)
+    if (mode == 's' or objects[index].overwrite or (previewObject.p1.equals(objects[index].p1)) == 0) return;
+
+    if (index >= 0) {
+        // Draws the preview object as a line if it is one
+        if (previewObject.type == 'l') {
+            rl.drawLineEx(previewObject.p1, previewObject.p2, lineThickness, previewObject.color);
+        }
+
+        // Draws the preview object as a circle if it is one
+        if (previewObject.type == 'c') {
+            const center: rl.Vector2 = previewObject.p1;
+            const radius: f32 = rl.Vector2.distance(previewObject.p1, previewObject.p2);
+
+            rl.drawRing(center, radius - (lineThickness / 2), radius + (lineThickness / 2), 0, 360, 1, previewObject.color);
+        }
+
+        // Draws the preview object as a rectangle if it is one
+        if (previewObject.type == 'r') {
+            var P1X: f32 = undefined;
+            var P1Y: f32 = undefined;
+            var P2X: f32 = undefined;
+            var P2Y: f32 = undefined;
+
+            if (previewObject.p1.x < previewObject.p2.x) {
+                P1X = previewObject.p1.x;
+                P2X = previewObject.p2.x;
+            } else {
+                P1X = previewObject.p2.x;
+                P2X = previewObject.p1.x;
+            }
+
+            if (previewObject.p1.y < previewObject.p2.y) {
+                P1Y = previewObject.p1.y;
+                P2Y = previewObject.p2.y;
+            } else {
+                P1Y = previewObject.p2.y;
+                P2Y = previewObject.p1.y;
+            }
+            const width: f32 = P2X - P1X;
+            const height: f32 = P2Y - P1Y;
+
+            rl.drawRectangleLinesEx(rl.Rectangle.init(P1X, P1Y, width, height), lineThickness, previewObject.color);
         }
     }
 }
@@ -493,6 +573,3 @@ fn modeChanger(key: rl.KeyboardKey) void {
         else => {},
     }
 }
-
-// an upcoming feature :O
-fn drawObjectPreview() void {}
